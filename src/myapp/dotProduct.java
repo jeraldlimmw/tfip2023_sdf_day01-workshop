ae37da0211423fac568899c1c940043b210b7414
Jerald Lim <jeraldlim@Jeralds-MacBook-Pro.local>
2023-02-02 09:05:50 +0800

    dot product solved

--- src/myapp/dotProduct.java
+++ src/myapp/dotProduct.java
@@ -1,20 +1,48 @@
 package myapp;
 
 import java.io.Console;
+import java.util.Arrays;
 
 public class dotProduct {
     
     public static void main(String args[]) {
-        Console cons = new console();
+        Console cons = System.console();
 
-        String equations = cons.readLine("How many equations: ");
+        String matrixEquations = cons.readLine("How many equations: ");
+        String matrixTerms = cons.readLine("How many terms:");
 
-        public int[][]
-        String terms = cons.readLine("How many terms:");
+        int equations = Integer.parseInt(matrixEquations);
+        int terms = Integer.parseInt(matrixTerms);
 
-        for int (i=0; i<equations; i++) {
-            String term = cons.readLine("How many terms:");    
+        int[][] matrix = new int[equations][terms];
+
+        System.out.println("Please enter the equation");
+        for (int i=0; i<equations; i++) {
+            String inputTerms = cons.readLine("%d)", i+1);
+            String[] termsStr = inputTerms.trim().split(" ");
+            for (int j=0; j<terms; j++) {
+                matrix[i][j] = Integer.parseInt(termsStr[j]);
+            }
         }
-    }
-    
+        
+        String inputWeights = cons.readLine("Please enter the weights: ");
+        
+        String[] weightsStr = inputWeights.trim().split(" ");
+        int[] weights = new int[terms];
+
+        for (int i=0; i<terms; i++) {
+            weights[i] = Integer.parseInt(weightsStr[i]);
+        }
+
+        int[] products = new int[equations];
+        for (int i=0; i<equations; i++) {
+            int product = 0;
+            for (int j=0; j<terms; j++) {
+                product += (matrix[i][j] * weights[j]);
+            }
+            products[i] = product;
+        }
+
+        System.out.println(Arrays.toString(products));
+    }   
 }
